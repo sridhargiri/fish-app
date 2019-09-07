75092a421b1b3c2230b8626fb9669ec8ee7760da
Elana Olson <elanaolson@gmail.com>
2019-09-07 15:59:07 -0700

    create route path for home page as fish profiles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FishProfileComponent } from './fish-profile/fish-profile.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FishProfileComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
