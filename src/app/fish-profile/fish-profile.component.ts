a90486f790e4c0685117996234d9659b0269ca3d
Elana Olson <elanaolson@gmail.com>
2019-09-07 18:41:51 -0700

    fix fish profile styling

--- src/app/fish-profile/fish-profile.component.ts
+++ src/app/fish-profile/fish-profile.component.ts
@@ -9,7 +9,7 @@ export class FishProfileComponent implements OnInit {
   fishies: string[][] = [
     ['Puffer Fish', 'Puffer fish can puff up to twice their size and excrete toxic poison from their spines.', 'assets/puffer_fish.jpg'], 
     ['Angel Fish', 'Angel fish have extended dorsal and anal fins, typically brightly colored or boldly striped.', 'assets/angel_fish.jpg'],
-    ['Clown Fish', 'Clown fish live in anemone, we know one - Nemo!', 'assets/clown_fish.jpg'], 
+    ['Clown Fish', 'Clown fish live in anemone, we know one - Nemo! Clown fish have a distinctive orange body with white stripes.', 'assets/clown_fish.jpg'], 
     ['Blue Orange Tang Fish', 'Tang fish get their pointed tails that resemble a tang scalpel, using it as self-defense when threatened in their natural coral reef habitat.', 'assets/blue_orange_tang_fish.jpg'], 
   ];
 
